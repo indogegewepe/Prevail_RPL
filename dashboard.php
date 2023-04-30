9a9fb4fbd0dcd6769e81616e3ddfcec2afae4fe7
Riyanadi120802 <Riyanadi083@gmail.com>
2023-04-30 19:24:54 +0700

    gantiDashboard

--- dashboard.php
+++ dashboard.php
@@ -1,75 +1,159 @@
 <?php
-require_once "core/init.php";
-
-if ( !isset($_SESSION['user'])){
-    header('Location: index.php');
-}
-
-require_once "view/headeradmin.php";
-
+require_once "ReadDataAPI.php";
 ?>
+<!DOCTYPE html>
+<!-- === Coding by CodingLab | www.codinglabweb.com === -->
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="style.css" />
+    <link
+      rel="stylesheet"
+      href="path/to/font-awesome/css/font-awesome.min.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
 
-<section class="dashboard">
-    <div class="top">
+    <!----===== Iconscout CSS ===== -->
+    <link
+      rel="stylesheet"
+      href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
+
+    <title>Admin Dashboard Panel</title>
+  </head>
+  <body>
+    <nav>
+      <div class="logo-name">
+        <div class="logo-image">
+          <img src="view\gambar\logo.png" alt="" />
+        </div>
+
+        <span class="logo_name">Cetakin</span>
+      </div>
+
+      <div class="menu-items">
+        <ul class="nav-links">
+          <li>
+            <a href="dashboard.php">
+              <i class="uil uil-create-dashboard"></i>
+              <span class="link-name">Layanan</span>
+            </a>
+          </li>
+          <li>
+            <a href="order.html">
+              <i class="uil uil-files-landscapes"></i>
+              <span class="link-name">Order</span>
+            </a>
+          </li>
+          <li>
+            <a href="status.php">
+              <i class="uil uil-chart"></i>
+              <span class="link-name">Status</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="uil uil-history"></i>
+              <span class="link-name">History</span>
+            </a>
+          </li>
+          <!-- <li>
+            <a href="#">
+              <i class="uil uil-comments"></i>
+              <span class="link-name">Comment</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="uil uil-share"></i>
+              <span class="link-name">Share</span>
+            </a>
+          </li> -->
+        </ul>
+
+        <ul class="logout-mode">
+          <li>
+            <a href="#">
+              <i class="uil uil-signout"></i>
+              <span class="link-name">Logout</span>
+            </a>
+          </li>
+
+          <li class="mode">
+            <a href="#">
+              <i class="uil uil-moon"></i>
+              <span class="link-name">Dark Mode</span>
+            </a>
+
+            <div class="mode-toggle">
+              <span class="switch"></span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <section class="dashboard">
+      <div class="top">
         <i class="uil uil-bars sidebar-toggle"></i>
 
         <div class="search-box">
-            <i class="uil uil-search"></i>
-            <input type="text" placeholder="Search here..." />
+          <i class="uil uil-search"></i>
+          <input type="text" placeholder="Search here..." />
         </div>
 
         <img src="images/profile.jpg" alt="" />
-    </div>
+      </div>
 
-    <div class="dash-content">
+      <div class="dash-content">
         <div class="overview">
-            <div class="title">
-                <i class="uil uil-tachometer-fast-alt"></i>
-                <span class="text">Layanan</span>
+          <div class="title">
+            <i class="uil uil-tachometer-fast-alt"></i>
+            <span class="text">Layanan</span>
+          </div>
+          <?php foreach($data1 as $row): ?>
+          <div class="boxes">
+            <div class="box box1">
+              <i class="fa fa-newspaper"></i>
+              <a href="#"><span class="text"><?= $row['namaLayanan']?></span></a>
+              <span class="text2">Rp. <?=$row['harga']?> / <?= $row['minPembelian']?> pcs</span>
             </div>
-
-            <div class="boxes">
-                <div class="box box1">
-                    <i class="fa fa-newspaper"></i>
-                    <a href="#"><span class="text">Brosur</span></a>
-                    <span class="text2">Rp.30.000/50pcs</span>
-                </div>
-                <div class="box box2">
-                    <i class="fa fa-calendar-check"></i>
-                    <a href="#"> <span class="text">Kalender</span></a>
-                    <span class="text2">Rp.30.000/50pcs</span>
-                </div>
-                <div class="box box3">
-                    <i class="fa fa-id-card"></i>
-                    <a href="#"><span class="text">Kartu Nama</span></a>
-                    <span class="text2">Rp.30.000/50pcs</span>
-                </div>
-                <div class="box box4">
-                    <i class="fa fa-hand-paper"></i>
-                    <a href="#"> <span class="text">Poster</span></a>
-                    <span class="text2">Rp.30.000/50pcs</span>
-                </div>
-                <div class="box box2">
-                    <i class="fa fa-note-sticky"></i>
-                    <a href="#"><span class="text">Black Note</span></a>
-                    <span class="text2">Rp.30.000/50pcs</span>
-                </div>
-                <div class="box box3">
-                    <i class="fa fa-file-invoice"></i>
-                    <a href="#"><span class="text">Invoice</span></a>
-                    <span class="text2">Rp.30.000/50pcs</span>
-                </div>
-                <div class="box box4">
-                    <i class="fa fa-message"></i>
-                    <a href="#"><span class="text">Amplop Surat</span></a>
-                    <span class="text2">Rp.30.000/50pcs</span>
-                </div>
-                <div class="box box1">
-                    <i class="fa fa-tags"></i>
-                    <a href="#"><span class="text">Kop Surat</span></a>
-                    <span class="text2">Rp.30.000/50pcs</span>
-                </div>
+          </div>
+            <?php endforeach; ?>
+          
+            <!-- <div class="box box2">
+              <i class="fa fa-calendar-check"></i>
+              <a href="#"> <span class="text">Kalender</span></a>
+              <span class="text2">Rp.30.000/50pcs</span>
             </div>
+            <div class="box box3">
+              <i class="fa fa-id-card"></i>
+              <a href="#"><span class="text">Kartu Nama</span></a>
+              <span class="text2">Rp.30.000/50pcs</span>
+            </div>
+            <div class="box box4">
+              <i class="fa fa-hand-paper"></i>
+              <a href="#"> <span class="text">Poster</span></a>
+              <span class="text2">Rp.30.000/50pcs</span>
+            </div>
+            <div class="box box2">
+              <i class="fa fa-note-sticky"></i>
+              <a href="#"><span class="text">Black Note</span></a>
+              <span class="text2">Rp.30.000/50pcs</span>
+            </div>
+            <div class="box box3">
+              <i class="fa fa-file-invoice"></i>
+              <a href="#"><span class="text">Invoice</span></a>
+              <span class="text2">Rp.30.000/50pcs</span>
+            </div>
+            <div class="box box4">
+              <i class="fa fa-message"></i>
+              <a href="#"><span class="text">Amplop Surat</span></a>
+              <span class="text2">Rp.30.000/50pcs</span>
+            </div> -->
         </div>
 
         <!-- <div class="activity">
@@ -132,9 +216,8 @@ require_once "view/headeradmin.php";
           </div>
         </div>
       </div> -->
-</section>
+    </section>
 
-<script src="../js/script.js"></script>
-</body>
-
+    <script src="view/js/script.js"></script>
+  </body>
 </html>
