9e5ef3d1588918c056eb78e26a17e24bf9cde84d
Riyanadi120802 <Riyanadi083@gmail.com>
2023-05-05 22:02:23 +0700

    rapiin sekali lagi

--- functions/user.php
+++ functions/user.php
@@ -37,29 +37,6 @@ function cek_data($username, $password){
     $db = new firebaseRDB($databaseURL);
     $data = $db->retrieve("customer");
     $data = json_decode($data, 1);
-    // global $conn;
-    // $username = mysqli_real_escape_string($conn, $username);
-    // $password = mysqli_real_escape_string($conn, $password);
-
-    // $query = "SELECT password FROM customer WHERE username = '$username'";
-
-    // $result = mysqli_query($conn, $query);
-    // $hash = mysqli_fetch_assoc($result)['password'];
-
-    // if (password_verify($password, $hash)){
-    //     $_SESSION['user'] = $username;
-    //     header('Location: dashboard.php');
-    // }else{
-    //     die("Password verification failed");
-    // }
-
-    // $query = "SELECT * FROM customer WHERE username = '$username'";
-
-    // if ( $result = mysqli_query($conn, $query) ){
-    //     if (mysqli_num_rows($result) == 0) return true;
-    //     else return false;
-    // }
-    // global $data3;
 
     if(is_array($data)){
         foreach($data as $id => $customer):
