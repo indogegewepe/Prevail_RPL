2681d1ccfadab6a64ba0492bdc6a9e87b1b91e50
Riyanadi120802 <Riyanadi083@gmail.com>
2023-05-30 10:31:33 +0700

    BaruOrder

--- order.php
+++ order.php
@@ -1,5 +1,6 @@
 <?php
 require_once "core/init.php";
+
 require_once "view/headeradmin.php";
 ?>
 
@@ -11,6 +12,29 @@ require_once "view/headeradmin.php";
           <div class="form first">
             <div class="details personal">
               <div class="fields">
+                <div class="input-field">
+                  <label>Nama</label>
+                  <input
+                    type="text"
+                    placeholder="Masukkan Nama Anda"
+                    required />
+                </div>
+
+                <div class="input-field">
+                  <label>Alamat</label>
+                  <input
+                    type="text"
+                    placeholder="Masukkan Alamat Anda"
+                    required />
+                </div>
+
+                <div class="input-field">
+                  <label>No Handphone / WA</label>
+                  <input
+                    type="number"
+                    placeholder="Masukkan No Handphone"
+                    required />
+                </div>
 
                 <div class="input-field">
                   <label>Jenis Layanan</label>
@@ -49,113 +73,6 @@ require_once "view/headeradmin.php";
               <span class="btnText">Next</span>
               <i class="uil uil-navigator"></i>
             </button>
-          </div>
-
-          <div class="form second">
-            <div class="details address">
-              <span class="title">Address Details</span>
-
-              <div class="fields">
-                <div class="input-field">
-                  <label>Address Type</label>
-                  <input
-                    type="text"
-                    placeholder="Permanent or Temporary"
-                    required />
-                </div>
-
-                <div class="input-field">
-                  <label>Nationality</label>
-                  <input type="text" placeholder="Enter nationality" required />
-                </div>
-
-                <div class="input-field">
-                  <label>State</label>
-                  <input type="text" placeholder="Enter your state" required />
-                </div>
-
-                <div class="input-field">
-                  <label>District</label>
-                  <input
-                    type="text"
-                    placeholder="Enter your district"
-                    required />
-                </div>
-
-                <div class="input-field">
-                  <label>Block Number</label>
-                  <input
-                    type="number"
-                    placeholder="Enter block number"
-                    required />
-                </div>
-
-                <div class="input-field">
-                  <label>Ward Number</label>
-                  <input
-                    type="number"
-                    placeholder="Enter ward number"
-                    required />
-                </div>
-              </div>
-            </div>
-
-            <div class="details family">
-              <span class="title">Family Details</span>
-
-              <div class="fields">
-                <div class="input-field">
-                  <label>Father Name</label>
-                  <input type="text" placeholder="Enter father name" required />
-                </div>
-
-                <div class="input-field">
-                  <label>Mother Name</label>
-                  <input type="text" placeholder="Enter mother name" required />
-                </div>
-
-                <div class="input-field">
-                  <label>Grandfather</label>
-                  <input
-                    type="text"
-                    placeholder="Enter grandfther name"
-                    required />
-                </div>
-
-                <div class="input-field">
-                  <label>Spouse Name</label>
-                  <input type="text" placeholder="Enter spouse name" required />
-                </div>
-
-                <div class="input-field">
-                  <label>Father in Law</label>
-                  <input
-                    type="text"
-                    placeholder="Father in law name"
-                    required />
-                </div>
-
-                <div class="input-field">
-                  <label>Mother in Law</label>
-                  <input
-                    type="text"
-                    placeholder="Mother in law name"
-                    required />
-                </div>
-              </div>
-
-              <div class="buttons">
-                <div class="backBtn">
-                  <i class="uil uil-navigator"></i>
-                  <span class="btnText">Back</span>
-                </div>
-
-                <button class="sumbit">
-                  <span class="btnText">Submit</span>
-                  <i class="uil uil-navigator"></i>
-                </button>
-              </div>
-            </div>
           </div>
         </form>
       </div>
