7230a6278151c65d7881b88c944dbab9c54b9eb9
Riyanadi120802 <Riyanadi083@gmail.com>
2023-07-11 15:07:50 +0700

    tambah

--- editLayanan.php
+++ editLayanan.php
@@ -1,5 +1,6 @@
 <?php
 require_once "core/init.php";
+require_once "view/SideBarAdmin.php";
 
 $db = new firebaseRDB($databaseURL);
 $id = $_GET['id'];
@@ -7,28 +8,35 @@ $retrieve = $db->retrieve("layanan/$id");
 $data = json_decode($retrieve, 1);
 
 ?>
-<form method="post" action="action_edit.php">
-    <table border="0" width="500">
-        <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td><input type="text" name="namaLayanan" value="<?php echo $data['namaLayanan'] ?>"></td>
-        </tr>
-        <tr>
-            <td>Harga</td>
-            <td>:</td>
-            <td><input type="number" name="harga" value="<?php echo $data['harga'] ?>"></td>
-        </tr>
-        <tr>
-            <td>Minimal Pembelian</td>
-            <td>:</td>
-            <td><input type="number" name="minPembelian" value="<?php echo $data['minPembelian'] ?>"></td>
-        </tr>
-        <tr>
-            <td>
-                <input type="hidden" name="id" value="<?php echo $id ?>">
-                <input type="submit" value="SAVE">
-            </td>
-        </tr>
-    </table>
-</form>
+
+<section class="dashboard">
+    <div class="dash-content">
+        <header style="font-size: 26px; font-weight: bold; color: blueviolet; ">Ubah Layanan</header>
+        <form method="post" action="action_edit.php">
+            <table border="0"  width="70%">
+                <!-- <div class="form-group input-group mb-3">
+                    <label for="nama">Nama</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Name" aria-label="email" aria-describedby="basic-addon2" required>
+                </div> -->
+                <tr>
+                    <td>Nama</td>
+                    <td><input type="text" class="form-control" name="namaLayanan" value="<?php echo $data['namaLayanan'] ?>"></td>
+                </tr>
+                <tr>
+                    <td>Harga</td>
+                    <td><input type="number" class="form-control" name="harga" value="<?php echo $data['harga'] ?>"></td>
+                </tr>
+                <tr>
+                    <td>Minimal Pembelian</td>
+                    <td><input type="number" class="form-control" name="minPembelian" value="<?php echo $data['minPembelian'] ?>"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="hidden" name="id" value="<?php echo $id ?>">
+                        <input class="btn btn-primary" type="submit" name="submit" value="Simpan">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</section>
